d1769de327b47824da7d654ebba956296dabfe02
Andre Mesquita <avmesquita@gmail.com>
2022-09-15 11:33:36 +0100

    try rebuild with server failure

--- CttService/server.js
+++ CttService/server.js
@@ -13,6 +13,7 @@ var swaggerUi = require('swagger-ui-express'),
     swaggerDocument = require('./swagger.json');
 
 var cors = require('cors');
+const { on } = require('nodemon');
 const app = express();
 app.use(cors());
 
@@ -33,15 +34,40 @@ app.get('/api/v1/distrito/:filter', (req, res) => {
 	.then(data => res.json(data)).catch(console.error);
 })
 
+// USING KNEX RAW TO EXECUTE A QUERY
+/*
+app.get('/api/v1/concelho/:filter', (req, res) => {
+
+	const filter = req.params.filter;
+	const queryJoin = "SELECT CNCLH.Id, CNCLH.CodigoDistrito, CNCLH.Codigo, CNCLH.Nome, DSTRT.Nome as Distrito " +
+					  "FROM Concelho CNCLH " +
+					  "       INNER JOIN Distrito DSTRT "
+					  "         ON DSTRT.Codigo = CNCLH.CodigoDistrito ";
+
+	if (filter !== '' && filter !== undefined && filter !== 'undefined' && filter !== {filter} && filter !== '{filter}') {
+		queryJoin = queryJoin + "WHERE Nome like '%" + filter + "%' ";
+	}
+	list = () => knex.raw(queryJoin);
+	
+	list()
+	.then(data => res.json(data)).catch(console.error);
+})
+*/
+
+// USING KNEX STRUCTURE (TOO FASTER THAN EXECUTE A QUERY)
 app.get('/api/v1/concelho/:filter', (req, res) => {
 
 	const filter = req.params.filter;
 
 	if (filter !== '' && filter !== undefined && filter !== 'undefined' && filter !== {filter} && filter !== '{filter}') {
-		list = () =>  knex('Concelho').select('Id','CodigoDistrito','Codigo','Nome')	   
+		list = () =>  knex('Concelho')
+					   .innerJoin("Distrito","Distrito.Codigo","Concelho.CodigoDistrito")
+					   .select('Concelho.Id','Concelho.CodigoDistrito','Concelho.Codigo','Concelho.Nome', 'Distrito.Nome as NomeDistrito')	   						
 					   .orWhere('Nome', 'like', '%'+filter+'%');
 	} else {
-		list = () =>  knex('Concelho').select('Id','CodigoDistrito','Codigo','Nome')
+		list = () =>  knex('Concelho')
+					   .innerJoin("Distrito","Distrito.Codigo","Concelho.CodigoDistrito")	
+					   .select('Concelho.Id','Concelho.CodigoDistrito','Concelho.Codigo','Concelho.Nome', 'Distrito.Nome as NomeDistrito');
 	}
 	
 	list()
@@ -53,29 +79,28 @@ app.get('/api/v1/apartado/:filter', (req, res) => {
 	const filter = req.params.filter;
 
 	if (filter !== '' && filter !== undefined && filter !== 'undefined' && filter !== {filter} && filter !== '{filter}') {
-		list = () =>  knex('Apartado')
-					   .select('Id','PostalOfficeIdentification','FirstPOBox','LastPOBox',
+		list = () =>  knex
+					   	.select('Id','PostalOfficeIdentification','FirstPOBox','LastPOBox',
 							   'PostalCode','PostalCodeExtension','PostalName','PostalCodeSpecial',
 							   'PostalCodeSpecialExtension','PostalNameSpecial')
-					   .where('PostalCode', 'like', '%'+filter+'%')
-					   .orWhere('FirstPOBox', 'like', '%'+filter+'%')
-					   .orWhere('LastPOBox', 'like', '%'+filter+'%')
-					   .orWhere('PostalCode', 'like', '%'+filter+'%')
-					   .orWhere('PostalName', 'like', '%'+filter+'%')
-					   .orWhere('PostalNameSpecial', 'like', '%'+filter+'%');					   
+					   	.from('Apartado')
+					   	.where('PostalCode', 'like', '%'+filter+'%')
+					   	.orWhere('FirstPOBox', 'like', '%'+filter+'%')
+					   	.orWhere('LastPOBox', 'like', '%'+filter+'%')
+					   	.orWhere('PostalCode', 'like', '%'+filter+'%')
+					   	.orWhere('PostalName', 'like', '%'+filter+'%')
+					   	.orWhere('PostalNameSpecial', 'like', '%'+filter+'%');					   
 	} else {
-		list = () =>  knex('Apartado')
+		list = () =>  knex
 						.select('Id','PostalOfficeIdentification','FirstPOBox','LastPOBox',
 								'PostalCode','PostalCodeExtension','PostalName','PostalCodeSpecial',
-								'PostalCodeSpecialExtension','PostalNameSpecial')						
+								'PostalCodeSpecialExtension','PostalNameSpecial')
+						.from('Apartado')
 	}
 	
 	list()
 	.then(data => res.json(data))
 });
-
-
-
 
 app.get('/api/v1/codigopostal/:filter', (req, res) => {
 
@@ -90,23 +115,53 @@ app.get('/api/v1/codigopostal/:filter', (req, res) => {
 	}
 
 	if (cp !== undefined && cp !== 'undefined') {
-		if (ext !== undefined && ext !== '') {			
-			list = () =>  knex('CodigoPostal')
-			.select('Id','CodigoDistrito','CodigoConcelho','CodigoLocalidade','NomeLocalidade','CodigoArteria','ArteriaTipo','PrimeiraPreposicao','ArteriaTitulo','SegundaPreposicao','ArteriaDesignacao','ArteriaInformacaoLocalZona','Troco','NumeroPorta','NomeCliente','NumeroCodigoPostal','NumeroExtensaoCodigoPostal','DesignacaoPostal')
-			   .where('NumeroCodigoPostal', cp)
-			.andWhere('NumeroExtensaoCodigoPostal', ext);
-		} else {			
-			list = () =>  knex('CodigoPostal')
-			.select('Id','CodigoDistrito','CodigoConcelho','CodigoLocalidade','NomeLocalidade','CodigoArteria','ArteriaTipo','PrimeiraPreposicao','ArteriaTitulo','SegundaPreposicao','ArteriaDesignacao','ArteriaInformacaoLocalZona','Troco','NumeroPorta','NomeCliente','NumeroCodigoPostal','NumeroExtensaoCodigoPostal','DesignacaoPostal')
-			   .where('NumeroCodigoPostal', cp.toString());
+		if (ext !== undefined && ext !== '') {				
+			list = () =>  knex
+			.select('CodigoPostal.Id','CodigoPostal.CodigoDistrito','CodigoPostal.CodigoConcelho','CodigoPostal.CodigoLocalidade',
+					'CodigoPostal.NomeLocalidade','CodigoPostal.CodigoArteria','CodigoPostal.ArteriaTipo','CodigoPostal.PrimeiraPreposicao',
+					'CodigoPostal.ArteriaTitulo','CodigoPostal.SegundaPreposicao','CodigoPostal.ArteriaDesignacao',
+					'CodigoPostal.ArteriaInformacaoLocalZona','CodigoPostal.Troco','CodigoPostal.NumeroPorta','CodigoPostal.NomeCliente',
+					'CodigoPostal.NumeroCodigoPostal','CodigoPostal.NumeroExtensaoCodigoPostal','CodigoPostal.DesignacaoPostal',
+					'Distrito.Nome as NomeDistrito', 'Concelho.Nome as NomeConcelho')
+			.from('CodigoPostal')
+			.innerJoin('Distrito','Distrito.Codigo','CodigoPostal.CodigoDistrito')
+			.innerJoin('Concelho', function() {
+				this.on('Concelho.Codigo', '=', 'CodigoPostal.CodigoConcelho')
+					.on('Concelho.CodigoDistrito', '=', 'CodigoPostal.CodigoDistrito')
+			})
+ 		    .where('CodigoPostal.NumeroCodigoPostal', cp.toString())
+			.andWhere('CodigoPostal.NumeroExtensaoCodigoPostal', ext.toString());
+		} else {						
+			list = () =>  knex
+			.select('CodigoPostal.Id','CodigoPostal.CodigoDistrito','CodigoPostal.CodigoConcelho','CodigoPostal.CodigoLocalidade',
+					'CodigoPostal.NomeLocalidade','CodigoPostal.CodigoArteria','CodigoPostal.ArteriaTipo','CodigoPostal.PrimeiraPreposicao',
+					'CodigoPostal.ArteriaTitulo','CodigoPostal.SegundaPreposicao','CodigoPostal.ArteriaDesignacao',
+					'CodigoPostal.ArteriaInformacaoLocalZona','CodigoPostal.Troco','CodigoPostal.NumeroPorta','CodigoPostal.NomeCliente',
+					'CodigoPostal.NumeroCodigoPostal','CodigoPostal.NumeroExtensaoCodigoPostal','CodigoPostal.DesignacaoPostal',
+					'Distrito.Nome as NomeDistrito', 'Concelho.Nome as NomeConcelho')
+			.from('CodigoPostal')
+			.innerJoin('Distrito','Distrito.Codigo','CodigoPostal.CodigoDistrito')
+			.innerJoin('Concelho', function() {
+				this.on('Concelho.Codigo', '=', 'CodigoPostal.CodigoConcelho')
+					.on('Concelho.CodigoDistrito', '=', 'CodigoPostal.CodigoDistrito')
+			})
+			.where('CodigoPostal.NumeroCodigoPostal', cp.toString());
 		}
-	} else {		
-		list = () =>  knex('CodigoPostal')
-						  .select('Id','CodigoDistrito','CodigoConcelho','CodigoLocalidade','NomeLocalidade',
-								  'CodigoArteria','ArteriaTipo','PrimeiraPreposicao','ArteriaTitulo',
-								  'SegundaPreposicao','ArteriaDesignacao','ArteriaInformacaoLocalZona',
-								  'Troco','NumeroPorta','NomeCliente','NumeroCodigoPostal','NumeroExtensaoCodigoPostal',
-								  'DesignacaoPostal').limit(20);
+	} else {				
+		list = () =>  knex		
+		.select('CodigoPostal.Id','CodigoPostal.CodigoDistrito','CodigoPostal.CodigoConcelho','CodigoPostal.CodigoLocalidade',
+				'CodigoPostal.NomeLocalidade','CodigoPostal.CodigoArteria','CodigoPostal.ArteriaTipo','CodigoPostal.PrimeiraPreposicao',
+				'CodigoPostal.ArteriaTitulo','CodigoPostal.SegundaPreposicao','CodigoPostal.ArteriaDesignacao',
+				'CodigoPostal.ArteriaInformacaoLocalZona','CodigoPostal.Troco','CodigoPostal.NumeroPorta','CodigoPostal.NomeCliente',
+				'CodigoPostal.NumeroCodigoPostal','CodigoPostal.NumeroExtensaoCodigoPostal','CodigoPostal.DesignacaoPostal',
+				'Distrito.Nome as NomeDistrito', 'Concelho.Nome as NomeConcelho')
+		.from('CodigoPostal')
+		.innerJoin('Distrito','Distrito.Codigo','CodigoPostal.CodigoDistrito')
+		.innerJoin('Concelho', function() {
+			this.on('Concelho.Codigo', '=', 'CodigoPostal.CodigoConcelho')
+				.on('Concelho.CodigoDistrito', '=', 'CodigoPostal.CodigoDistrito')
+		})
+		.limit(20);
 	}
 	
 	list()
@@ -117,23 +172,42 @@ app.get('/api/v1/morada/:filter', (req, res) => {
 
 	const filter = req.params.filter;
 
-	if (filter !== undefined && filter !== 'undefined') {			
-			list = () =>  knex('CodigoPostal')
-			.select('Id','CodigoDistrito','CodigoConcelho','CodigoLocalidade','NomeLocalidade','CodigoArteria','ArteriaTipo','PrimeiraPreposicao','ArteriaTitulo','SegundaPreposicao','ArteriaDesignacao','ArteriaInformacaoLocalZona','Troco','NumeroPorta','NomeCliente','NumeroCodigoPostal','NumeroExtensaoCodigoPostal','DesignacaoPostal')
-			.where('ArteriaTitulo', 'like', '%'+filter+'%')
-			.orWhere('NomeLocalidade', 'like', '%'+filter+'%')
-			.orWhere('ArteriaDesignacao', 'like', '%'+filter+'%')			
-			.orWhere('ArteriaTipo', 'like', '%'+filter+'%')
-			.orWhere('NomeCliente', 'like', '%'+filter+'%')
-			.orWhere('DesignacaoPostal', 'like', '%'+filter+'%')
-			.orWhere('ArteriaInformacaoLocalZona', 'like', '%'+filter+'%');					   
-	} else {		
-		list = () =>  knex('CodigoPostal')
-						  .select('Id','CodigoDistrito','CodigoConcelho','CodigoLocalidade','NomeLocalidade',
-								  'CodigoArteria','ArteriaTipo','PrimeiraPreposicao','ArteriaTitulo',
-								  'SegundaPreposicao','ArteriaDesignacao','ArteriaInformacaoLocalZona',
-								  'Troco','NumeroPorta','NomeCliente','NumeroCodigoPostal','NumeroExtensaoCodigoPostal',
-								  'DesignacaoPostal').limit(20);
+	if (filter !== '' && filter !== undefined && filter !== 'undefined' && filter !== {filter} && filter !== '{filter}') {		
+			list = () =>  knex
+			.select('CodigoPostal.Id','CodigoPostal.CodigoDistrito','CodigoPostal.CodigoConcelho','CodigoPostal.CodigoLocalidade',
+					'CodigoPostal.NomeLocalidade','CodigoPostal.CodigoArteria','CodigoPostal.ArteriaTipo','CodigoPostal.PrimeiraPreposicao',
+					'CodigoPostal.ArteriaTitulo','CodigoPostal.SegundaPreposicao','CodigoPostal.ArteriaDesignacao',
+					'CodigoPostal.ArteriaInformacaoLocalZona','CodigoPostal.Troco','CodigoPostal.NumeroPorta','CodigoPostal.NomeCliente',
+					'CodigoPostal.NumeroCodigoPostal','CodigoPostal.NumeroExtensaoCodigoPostal','CodigoPostal.DesignacaoPostal',
+					'Distrito.Nome as NomeDistrito', 'Concelho.Nome as NomeConcelho')
+			.from('CodigoPostal')
+			.innerJoin('Distrito','Distrito.Codigo','CodigoPostal.CodigoDistrito')
+			.innerJoin('Concelho', function() {
+				this.on('Concelho.Codigo', '=', 'CodigoPostal.CodigoConcelho')
+					.on('Concelho.CodigoDistrito', '=', 'CodigoPostal.CodigoDistrito')
+			})
+			.where('CodigoPostal.ArteriaTitulo', 'like', '%'+filter+'%')
+			.orWhere('CodigoPostal.NomeLocalidade', 'like', '%'+filter+'%')
+			.orWhere('CodigoPostal.ArteriaDesignacao', 'like', '%'+filter+'%')			
+			.orWhere('CodigoPostal.ArteriaTipo', 'like', '%'+filter+'%')
+			.orWhere('CodigoPostal.NomeCliente', 'like', '%'+filter+'%')
+			.orWhere('CodigoPostal.DesignacaoPostal', 'like', '%'+filter+'%')
+			.orWhere('CodigoPostal.ArteriaInformacaoLocalZona', 'like', '%'+filter+'%');					   
+	} else {				
+		list = () =>  knex
+		.select('CodigoPostal.Id','CodigoPostal.CodigoDistrito','CodigoPostal.CodigoConcelho','CodigoPostal.CodigoLocalidade',
+				'CodigoPostal.NomeLocalidade','CodigoPostal.CodigoArteria','CodigoPostal.ArteriaTipo','CodigoPostal.PrimeiraPreposicao',
+				'CodigoPostal.ArteriaTitulo','CodigoPostal.SegundaPreposicao','CodigoPostal.ArteriaDesignacao',
+				'CodigoPostal.ArteriaInformacaoLocalZona','CodigoPostal.Troco','CodigoPostal.NumeroPorta','CodigoPostal.NomeCliente',
+				'CodigoPostal.NumeroCodigoPostal','CodigoPostal.NumeroExtensaoCodigoPostal','CodigoPostal.DesignacaoPostal',
+				'Distrito.Nome as NomeDistrito', 'Concelho.Nome as NomeConcelho')	
+		.from('CodigoPostal')
+		.innerJoin('Distrito','Distrito.Codigo','CodigoPostal.CodigoDistrito')
+		.innerJoin('Concelho', function() {
+			this.on('Concelho.Codigo', '=', 'CodigoPostal.CodigoConcelho')
+				.on('Concelho.CodigoDistrito', '=', 'CodigoPostal.CodigoDistrito')
+		})
+	  .limit(20);
 	}
 	
 	list()
